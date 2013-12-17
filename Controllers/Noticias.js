ceab624710376b1a4a5e65345d6cd32c90fe8863
Igor C. de Paula <igordepaula@adminweb.com.br>
2013-12-17 08:17:40 -0200

    quit

--- Controllers/Noticias.js
+++ Controllers/Noticias.js
@@ -2,7 +2,7 @@
 var NoticiaAppControllers = angular.module('Controllers', []);
 
 var NoticiasController = function($scope, $routeParams, NoticiaServices) {
-    $scope.noticia = NoticiaServices.get({idnoticia: $routeParams.id},function(phone) {
+    $scope.noticia = NoticiaServices.getNoticia({idnoticia: $routeParams.id},function(phone) {
         $scope.noticia = phone;
     });
 
--- Services/Noticias.js
+++ Services/Noticias.js
@@ -2,6 +2,6 @@ var NoticiasAppServices = angular.module('Services', ['ngResource']);
 
 var NoticiaServices = function($resource) {
     return $resource('http://www.irmnet.com.br/api/getnoticia?noticia=:idnoticia', {}, {
-        get: {method: 'GET', params: {}}
+        getNoticia: {method: 'GET', params: {}}
     });
 };
